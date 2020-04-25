2eb32cc17229dba4558f6d0ca62a0c7e0252463e
xiaoguo_ge@yahoo.com <xiaoguo_ge@yahoo.com>
2020-04-25 20:51:48 +1000

    cleaned up table name, bucket name, access-key id

--- Program.cs
+++ Program.cs
@@ -9,16 +9,16 @@ namespace AthenaDemo
     {
         static async Task Main(string[] args)
         {
-            var client = new AmazonAthenaClient("AKIASR7SM37VRBTWH7OS", "xxx-xxx-xxxxxxxxxx", RegionEndpoint.APSoutheast2);
+            var client = new AmazonAthenaClient("access-key-id", "access-key", RegionEndpoint.APSoutheast2);
             var queryRequest = new StartQueryExecutionRequest
             {
-                QueryString = "select * from cartoorates.lon_eod",
+                QueryString = "select * from athena-database-name.table-name",
                 ResultConfiguration = new ResultConfiguration
                 {
-                    OutputLocation = "s3://athena-query-result-cartoo/"
+                    OutputLocation = "s3://s3-bucket-name-for-query-result/"
                 }
             };
-            var result = await client.QueryAsyncLight(queryRequest, 1);
+            var result = await client.QueryAsyncLight(queryRequest, 5);
 
             var rows = result.ResultSet.Rows;
         }
